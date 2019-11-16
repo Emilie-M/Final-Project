84536c70132351b19cf223abf69e9cc7ecd311e8
Emilie-M <54450889+Emilie-M@users.noreply.github.com>
2019-11-16 11:20:06 -0500

    Setup first room

---
+++ Blitz Breakout/Room1.java
@@ -0,0 +1,24 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/**
+ * Write a description of class Room1 here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class Room1 extends SimulationWorld
+{
+    /**
+     * Constructor for objects of class Room1.
+     * 
+     */
+    public Room1()
+    {
+        super("gameplay.wav", 800, 600, new Point2D(0.0, 0.0), 20);
+    }
+    
+    public void act()
+    {
+        super.act();
+    }
+}
