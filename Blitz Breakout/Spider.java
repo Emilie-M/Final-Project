f022a263d60a079a414dc889e53c0441cd3c73c7
Emilie-M <54450889+Emilie-M@users.noreply.github.com>
2019-11-16 13:36:15 -0500

    Spider movement

--- Blitz Breakout/Spider.java
+++ Blitz Breakout/Spider.java
@@ -8,12 +8,55 @@ import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
  */
 public class Spider extends SimulationActor
 {
-    public Spider()
+    protected final static double SPIDER_SPEED = 2.0;
+    protected double minHeight;
+    protected double maxHeight;
+    
+    public Spider(double minHeight,double maxHeight)
     {
+        super();
+        this.minHeight = minHeight;
+        this.maxHeight = maxHeight;
+        
+        velocity = new Vector2D(0.0, SPIDER_SPEED);
     }
     
-    public void act()
+    public void act() 
     {
         super.act();
+        
+        if (position.getY() > maxHeight)
+        {
+            velocity = new Vector2D(0.0, -SPIDER_SPEED);
+        }
+        
+        if (position.getY() < minHeight)
+        {
+            velocity = new Vector2D(0.0, SPIDER_SPEED);
+        }
+    }
+    
+    public int getWidth()
+    {
+        if (getImage() != null)
+        {
+            return getImage().getWidth();
+        }
+        else
+        {
+            return 0;
+        }            
+    }
+
+    public int getHeight()
+    {
+        if (getImage() != null)
+        {
+            return getImage().getHeight();
+        }
+        else
+        {
+            return 0;
+        }            
     }
 }
