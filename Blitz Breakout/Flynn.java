df467c8e7b7b64a462ae2ef39aa61641be812cb3
akashstar12 <akash_star12@hotmail.com>
2019-11-16 14:36:19 -0500

    SpriteSheet1stTry

--- Blitz Breakout/Flynn.java
+++ Blitz Breakout/Flynn.java
@@ -8,12 +8,95 @@ import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
  */
 public class Flynn extends SimulationActor
 {
-    /**
-     * Act - do whatever the Flynn wants to do. This method is called whenever
-     * the 'Act' or 'Run' button gets pressed in the environment.
-     */
+    protected static final SpriteSheet standRight = new SpriteSheet
+    ("JumpInitial.png", 1, 6);
+    protected static final SpriteSheet runRight = new SpriteSheet
+    ("SpriteRunSequenceRight.png", 1, 6);
+    protected static final SpriteSheet runLeft = new SpriteSheet
+    ("SpriteJumpSequenceLeft.png", 1, 6);
+    protected static final double SPRITE_FRAMES_DURATION = 0.1;
+    protected static final double FLYNN_VELOCITY = 1.6;
+    protected int currentRow;
+    protected int currentColumn;
+    protected double currentFrameTime;
+    
+    public Flynn()
+    {
+        super(null, new Vector2D(0,0), new Vector2D(0, GRAVITY));
+        currentRow = 1;
+        currentColumn = 0;
+        currentFrameTime = 0.0;
+        
+        GreenfootImage sprite = standRight.getSprite(currentRow,currentColumn);
+        setImage(sprite);
+    }
+    
     public void act() 
     {
-        // Add your action code here.
+        super.act();
+        
+        moveOnPlatform();
+        updateFrame();
     }    
+    
+    //public void moveOnPlatform()
+    //{
+        //if (onPlatform == true)
+        //{
+            //if (velocity.getX() == 0)
+            //{
+                //velocity.setX(SKELETON_VELOCITY);
+                //currentRow = 3;
+            //}
+        //}
+        //else
+        //{
+            //velocity.setX(-velocity.getX());
+            
+            //if (velocity.getX() > 0.0)
+            //{
+                //currentRow = 3;
+            //}
+            //else
+            //{
+                //currentRow = 1;
+            //}           
+        //}
+    //}
+    
+    public void moveFromKeyboard()
+    {
+        if (Greenfoot.isKeyDown("a"))
+        {
+            // Moving left in the second row in the spritesheet
+            currentRow = 1;
+        }
+        if (Greenfoot.isKeyDown("s"))
+        {
+            // Moving down in the second row in the spritesheet
+            currentRow = 2;
+        }
+        if (Greenfoot.isKeyDown("d"))
+        {
+            // Moving left in the second row in the spritesheet
+            currentRow = 3;
+        }
+        if (Greenfoot.isKeyDown("w"))
+        {
+            // Moving up in the second row in the spritesheet
+            currentRow = 0;
+        }
+    }
+    
+    public void updateFrame()
+    {
+        currentFrameTime += getSimulationWorld().getTimeStepDuration();
+        if (currentFrameTime > SPRITE_FRAMES_DURATION)
+        {
+            currentColumn = (currentColumn + 1) % spriteSheet.getColumns();
+            currentFrameTime -= SPRITE_FRAMES_DURATION;
+        }
+        
+        setImage(spriteSheet.getSprite(currentRow, currentColumn));
+    }
 }
