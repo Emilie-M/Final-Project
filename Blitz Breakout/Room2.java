b90998c6eccd2d887bbb944bcc641d3dd5d2ce9f
akashstar12 <akash_star12@hotmail.com>
2019-11-20 21:17:49 -0500

    CollisionWithDoor + NewDoorColor ideas
    yepyep

---
+++ Blitz Breakout/Room2.java
@@ -0,0 +1,24 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/**
+ * Write a description of class Room2 here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class Room2 extends SimulationWorld
+{
+
+    /**
+     * Constructor for objects of class Room2.
+     * 
+     */
+    public Room2()
+    {
+        super(900, 768, new Point2D(8.0, 6.0), 16.0); 
+    }
+    public void act()
+    {
+        super.act();
+    }
+}
