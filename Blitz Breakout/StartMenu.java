5d0724bdd10f147d6a2d16c7c9c29bb1d0dc8e26
Emilie-M <54450889+Emilie-M@users.noreply.github.com>
2019-11-24 13:05:44 -0500

    False

--- Blitz Breakout/StartMenu.java
+++ Blitz Breakout/StartMenu.java
@@ -16,4 +16,13 @@ public class StartMenu extends SimulationWorld
     {
         super("", 900, 768, new Point2D(8.0, 6.0), 16.0);
     }
+    
+    public void act()
+    {
+        super.act();
+        if (Greenfoot.isKeyDown("space"))
+        {
+            transitionToWorld(new Room1(false));
+        }
+    }
 }
