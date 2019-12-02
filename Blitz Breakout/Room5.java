cf6f38712844b12acc6048552517d830282f138f
Emilie-M <54450889+Emilie-M@users.noreply.github.com>
2019-12-01 19:21:29 -0500

    distance keys

--- Blitz Breakout/Room5.java
+++ Blitz Breakout/Room5.java
@@ -69,32 +69,32 @@ public class Room5 extends SimulationWorld
         }
         if (isGoldKeyAcquired2 && b < 1) 
         {
-            addObject(new GoldKey(), 60, 735);
+            addObject(new GoldKey(), 62, 735);
             b++;
         }
         if (isGreenKeyAcquired2 && c < 1) 
         {
-            addObject(new GreenKey(), 62, 735);
+            addObject(new GreenKey(), 66, 735);
             c++;
         }
         if (isOrangeKeyAcquired2 && d < 1) 
         {
-            addObject(new OrangeKey(), 64, 735);
+            addObject(new OrangeKey(), 70, 735);
             d++;
         }
         if (isPinkKeyAcquired2 && e < 1) 
         {
-            addObject(new PinkKey(), 66,735);
+            addObject(new PinkKey(), 74,735);
             e++;
         }
         if (isRedKeyAcquired2 && f < 1) 
         {
-            addObject(new RedKey(), 68, 735);
+            addObject(new RedKey(), 78, 735);
             f++;
         }
         if (isSilverKeyAcquired2 && g < 1) 
         {
-            addObject(new SilverKey(), 70, 735);
+            addObject(new SilverKey(), 82, 735);
             g++;
         }
         
